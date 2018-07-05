5d745161c6aeab6c4908170fcc13c7aa11f81f2d
Matias <mmffalkiewicz@gmail.com>
2018-07-05 18:48:36 -0300

    Initial commit

---
+++ app/src/main/java/com/example/windows10/leerimagen/AcercaDe2.java
@@ -0,0 +1,18 @@
+package com.example.windows10.leerimagen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Windows 10 on 27/04/2017.
+ */
+
+public class AcercaDe2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acercade2);
+        setTitle("Acerca de ...");
+    }
+}
